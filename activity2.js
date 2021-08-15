0eb1d19e31c5e615de9bc08cf93c05973bd03f05
1234Richard <82384955+1234Richard@users.noreply.github.com>
2021-08-15 17:51:49 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,8 @@
+score = 0;
+function getScore() {
+    score = localStorage.getItem("score");
+    document.getElementById("update").innerHTML = "score:" + score;} 
+
+function back() {
+    window.location = "activity_1.html"
+} 
